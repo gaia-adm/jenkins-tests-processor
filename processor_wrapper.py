67ad42925775ae1096aed4e4b31c83f304b09f83
Jaroslav Libak <jaroslav.libak@hp.com>
2015-08-11 15:05:02 +0200

    added processor_wrapper.py to allow usage of __pycache__ for processor.py

---
+++ processor_wrapper.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3.4
+# This file exists to allow usage of __pycache__ for processor.py.
+# Python doesn't cache pyc file for executed script, therefore we have this minimal wrapper.
+import processor
